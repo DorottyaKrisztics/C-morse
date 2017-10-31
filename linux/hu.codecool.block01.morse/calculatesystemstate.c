7ad1aa5f41c3e94390ead2a93a46c2b2e2f54d9d
DorottyaKrisztics <dorka713@gmail.com>
2017-10-31 10:51:25 +0100

    Environment data convert from integer to float

--- linux/hu.codecool.block01.morse/calculatesystemstate.c
+++ linux/hu.codecool.block01.morse/calculatesystemstate.c
@@ -9,4 +9,28 @@
  * env: contains environment data and calculated state will be stored in it
  */
 void CalculateSystemState(EnvironmentData *env) {
+
+	env->state = OK;
+	float acc_x = intToFloat(env->acc_x);
+	float acc_y = intToFloat(env->acc_y);
+	float acc_z = intToFloat(env->acc_z);
+
+		if (acc_x > 2 || acc_y > 2 || acc_z > 2 || (env->acc_x + env->acc_y + env->acc_z) / 10 > 3){
+			env->state = HIGH_ACCELERATION;
+			if (env->temp > 34){
+				env->state = HIGH_ACCELERATION_AND_TEMP;
+			}
+		} else if (env->temp > 34){
+			env->state = HIGH_TEMP;
+		}
+
 }
+
+float intToFloat(int input) {
+    float modulo = input % 10;
+    int number = input / 10;
+
+    return number + modulo / 10;
+}
+
+
